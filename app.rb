c498c0488683c333680e8e55f0108665de75c323
dan-cl <djclissold@yahoo.co.uk>
2016-07-18 14:34:30 +0100

    7. Start and restart

--- app.rb
+++ app.rb
@@ -1,1 +1,23 @@
 require 'sinatra'
+
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello world"
+end
+
+get '/secret' do
+  "super secret webpage"
+end
+
+get '/test' do
+  "does this work?"
+end
+
+get '/banana' do
+  "this is not a banana"
+end
+
+get '/lastone' do
+  "the last try"
+end
